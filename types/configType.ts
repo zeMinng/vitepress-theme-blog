023ad03490cf38881711f04621f7c4499000d5d5
zeMing <2439340964@qq.com>
2026-03-04 10:47:22 +0800

    feat: init

--- types/configType.ts
+++ types/configType.ts
@@ -1,16 +1,17 @@
-// import { DefaultTheme } from 'vitepress'
+import { DefaultTheme } from 'vitepress'
 
 /**
- * @description 站点配置
+ * @description VitePress 配置
  */
 export interface VitePressConfigType {
   author: string // 作者
   watermark: boolean // 水印
   ogImage: string // logo
   siteConfig: SiteConfig // 站点配置
-  themeConfig: any
+  themeConfig: DefaultTheme.Config & themeConfig
 }
 
+// 站点配置
 interface SiteConfig {
   title: string // 站点元数据
   description: string // 站点的描述。这将呈现为页面 HTML 中的 <meta> 标签
@@ -19,9 +20,7 @@ interface SiteConfig {
   lastUpdated: boolean // 使用 Git 获取每个页面的最后更新时间戳，可通过 useData 访问
 }
 
-// interface themeConfig {
-//   siteTitle: string | boolean // 是否显示站点名称
-//   logo: string // logo
-//   nav: Array<{ text: string; link: string }> // 导航栏
-//   socialLinks: Array<{ icon: string; link: string }> // 社交链接
-// }
+// 主题配置
+interface themeConfig {
+  
+}
